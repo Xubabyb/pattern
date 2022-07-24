fff9cbefe119c8c727b0ee36ce7088d1e470d769
Dmitriy Shidlovskiy <xubabyb@gmail.com>
2022-07-24 14:58:18 +0300

    add factory Thinking Java

---
+++ src/my/home/pattren/generative/factory/DemoFactoryThinkingJava.java
@@ -0,0 +1,56 @@
+package my.home.pattren.generative.factory;
+
+public class DemoFactoryThinkingJava {
+    public static void main(String[] args) {
+        serviceConsumer(ServiceAImpl.factory);
+
+    }
+    public static void serviceConsumer(ServiceFactory factory){
+        Service service = factory.getService();
+        service.methodA();
+        service.methodB();
+    }
+}
+
+interface Service {
+    void methodA();
+
+    void methodB();
+}
+
+interface ServiceFactory {
+    Service getService();
+}
+
+class ServiceAImpl implements Service {
+
+   public static ServiceFactory factory = ServiceAImpl::new;
+
+
+    @Override
+    public void methodA() {
+        System.out.println("Service A method A");
+    }
+
+    @Override
+    public void methodB() {
+        System.out.println("Service A  method B");
+
+    }
+}
+class ServiceBImpl implements Service {
+
+    public static ServiceFactory factory = ServiceBImpl::new;
+
+
+    @Override
+    public void methodA() {
+        System.out.println("Service B method A");
+    }
+
+    @Override
+    public void methodB() {
+        System.out.println("Service B method B");
+
+    }
+}
